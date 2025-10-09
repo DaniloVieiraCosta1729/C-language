886400cfdfe10f1fc3c208bc146553c70fa683fc
Danilo Vieria Costa <danilo.11235@outlook.com>
2025-10-08 22:46:31 -0300

    Prepara o menu.

--- Modern C/level0/c-quol_light.c
+++ Modern C/level0/c-quol_light.c
@@ -1,5 +1,8 @@
 #include <sqlite3.h>
 #include <stdio.h>
+#include <unistd.h>
+
+#define PULA printf("\n");
 
 sqlite3 * preparar(){
     sqlite3 * db;
@@ -16,6 +19,65 @@ sqlite3 * preparar(){
     
 }
 
+int menu()
+{
+    printf("================ T E R M I N A L  -- T O D O ================\n\n");
+
+    char c;
+    while (1)
+    {
+        printf("Select a option: \n");
+        printf("[A]dd, [L]ist, [C]omplete, [U]pdate, [R]emove, [Q]uit\n>");
+
+        while ((c = getchar()) != '\n')
+        {
+            printf(" Loading . . .\n");
+            sleep(1);
+        
+        
+            PULA
+
+            switch (c)
+            {
+            case 'A':
+            case 'a':
+                printf("New item added.\n");
+                break;
+            
+            case 'L':
+            case 'l':
+                printf("Tasks listed.\n");
+                break;
+
+            case 'C':
+            case 'c':
+                printf("ToDo Completed.\n");
+                break;
+
+            case 'U':
+            case 'u':
+                printf("Database Updated.\n");
+                break;
+            
+            case 'R':
+            case 'r':
+                printf("Item removed.\n");
+                break;
+
+            case 'Q':
+            case 'q':
+                printf("Thank you for using the ToDo-Teminal.\n");
+                return 0;
+
+            default:
+                printf("Invalid input.");
+                break;
+            }
+        }
+    }
+    
+}
+
 int main(int argc, char const *argv[])
 {
     sqlite3 * db = preparar();
@@ -29,6 +91,8 @@ int main(int argc, char const *argv[])
     printf("Db opened and ready.\n");
 
     sqlite3_close(db);
+
+    menu();
 
     return 0;
 }

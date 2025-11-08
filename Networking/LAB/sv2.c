064326b87356c2e80022321ea857dc98a2efd1a4
Danilo Vieria Costa <danilo.11235@outlook.com>
2025-11-08 14:28:42 -0300

    Tests an even simpler version of tcp server.

---
+++ Networking/LAB/sv2.c
@@ -0,0 +1,60 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/sendfile.h>
+
+int main(int argc, char const *argv[])
+{
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in saddr, caddr;
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(8080);
+    saddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(s, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
+    {
+        perror("Erro no bind.");
+    }
+
+    if (listen(s, 5) < 0)
+    {
+        perror("Erro no listen.");
+    }   
+
+    socklen_t addrlen = sizeof(caddr);
+
+    int c = accept(s, (struct sockaddr*)&caddr, (socklen_t *)&addrlen);
+
+    char buffer[1000];
+
+    read(c, buffer, 999);
+
+    buffer[1000] = '\0';
+
+    int arqTexto = open("sv2_text.txt", O_CREAT | O_RDWR, 0644);
+
+    write(arqTexto, buffer, strlen(buffer));
+
+    char * html;
+
+    char cab[500];
+
+    html = "<!DOCTYPE html><html><head><title>Front-end GUI</title></head><body><h1>Ola Browser!!@!</h1></body></html>";
+
+    sprintf(cab, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: %ld\r\nConnection: close\r\n\r\n%s", strlen(html), html);
+
+    send(c, cab, strlen(cab), 0);
+
+    close(arqTexto);
+    close(c);
+    close(s);    
+
+    return 0;
+}
